98dcf1d19367fc28da994b99ae9bf8f3f792d7b0
remem <rememberfox00@gmail.com>
2023-04-25 15:30:54 +0300

    header-visual

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Navbar from "./navbar/Navbar.jsx";
 import styles from './header.module.scss';
 import PageSwiper from "./swiper/pageSwiper/PageSwiper.jsx";
-
-
+import {Container} from "react-bootstrap";
 
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <Navbar />
-            <PageSwiper/>
+            <Navbar/>
+            <Container>
+                <PageSwiper/>
+            </Container>
         </header>
     );
 };
